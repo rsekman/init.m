5a94e53aac942a1596cfa4594cdea4558ab19da0
Robin Ekman <robin.seth.ekman@gmail.com>
2023-08-18 07:51:15 +0200

    initial commit

---
+++ init.m
@@ -0,0 +1,42 @@
+(* ::Package:: *)
+
+(** User Mathematica initialization file **)
+
+
+
+On[Assert];
+
+
+Needs["MaTeX`"];
+SetOptions[MaTeX,
+	FontSize -> 20,
+	"Preamble" -> {"\\usepackage{siunitx}"}
+ ];
+
+
+Begin["Ekman`"];
+StyleOptions = {
+	FontSize->24,
+	FontFamily ->"Latin Modern Roman",
+	FontColor->Black
+};
+PlotOptions = {
+	ImageSize->Full,
+	BaseStyle->StyleOptions,
+	LabelStyle->StyleOptions,
+	TicksStyle->StyleOptions,
+	
+	PlotRangePadding->None
+};
+SetOptions[
+	{Plot,LogPlot,LogLinearPlot,LogLogPlot,
+	ListPlot,ListLogPlot,ListLogLinearPlot,ListLogLogPlot,
+	DateListPlot,StackedListPlot,StackedDateListPlot
+	},
+	PlotOptions
+];
+SetOptions[
+	{PointLegend,LineLegend,BarLegend,SwatchLegend},
+	LabelStyle->StyleOptions
+];
+End[];
