8ae94205d601d26a33bd51bc6bedfded53591ee6
Robin Ekman <robin.seth.ekman@gmail.com>
2023-09-29 04:18:47 +0200

    linting: indent, spaces after commas

--- init.m
+++ init.m
@@ -9,34 +9,35 @@ On[Assert];
 
 Needs["MaTeX`"];
 SetOptions[MaTeX,
-	FontSize -> 20,
-	"Preamble" -> {"\\usepackage{siunitx}"}
+    FontSize -> 20,
+    "Preamble" -> {"\\usepackage{siunitx}"}
  ];
 
 
 Begin["Ekman`"];
 StyleOptions = {
-	FontSize->24,
-	FontFamily ->"Latin Modern Roman",
-	FontColor->Black
+    FontSize->24,
+    FontFamily ->"Latin Modern Roman",
+    FontColor->Black
 };
 PlotOptions = {
-	ImageSize->Full,
-	BaseStyle->StyleOptions,
-	LabelStyle->StyleOptions,
-	TicksStyle->StyleOptions,
-	
-	PlotRangePadding->None
+    ImageSize->Full,
+    BaseStyle->StyleOptions,
+    LabelStyle->StyleOptions,
+    TicksStyle->StyleOptions,
+    PlotRangePadding->None,
+    PlotStyle->Thick
 };
 SetOptions[
-	{Plot,LogPlot,LogLinearPlot,LogLogPlot,
-	ListPlot,ListLogPlot,ListLogLinearPlot,ListLogLogPlot,
-	DateListPlot,StackedListPlot,StackedDateListPlot
-	},
-	PlotOptions
+    {
+        Plot, LogPlot, LogLinearPlot, LogLogPlot,
+        ListPlot, ListLogPlot, ListLogLinearPlot, ListLogLogPlot,
+        DateListPlot, StackedListPlot, StackedDateListPlot
+    },
+PlotOptions
 ];
 SetOptions[
-	{PointLegend,LineLegend,BarLegend,SwatchLegend},
-	LabelStyle->StyleOptions
+    {PointLegend, LineLegend, BarLegend, SwatchLegend},
+    LabelStyle->StyleOptions
 ];
 End[];
